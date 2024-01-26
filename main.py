3958b6ba7b1c948449c7031ece5c83a16b1d6151
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2024-01-26 16:14:11 +0300

    копии файлов

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return f"""
+    <p>сайт начиающего верстальщика. Есть ссылки: </p>
         <a href={url_for('index')}>Сайт</a>
+        <a href={url_for('start')}>Стартовая стр</a>
+        <a href={url_for('base')}>База</a>
     """
 
 @app.route('/index')
 def index():
     return render_template('index.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+
+
+@app.route('/start')
+def start():
+    return render_template('start.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+
+
+
+@app.route('/base')
+def base():
+    return render_template('base.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+
 
 
 @app.route('/day-<num>')
