3e29ef47d907e16eed6651e9ace2a63f4ef2f7fb
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2024-02-02 16:13:16 +0300

    форма дляобращения и регистрации

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
+
+
+
 
 @app.route('/')
 def hello():
@@ -10,6 +12,8 @@ def hello():
         <a href={url_for('index')}>Сайт</a>
         <a href={url_for('start')}>Стартовая стр</a>
         <a href={url_for('base')}>База</a>
+        <a href={url_for('form')}>Форма чрт</a>
+        <a href={url_for('login')}>Регистрация</a>
     """
 
 @app.route('/index')
@@ -25,7 +29,23 @@ def start():
 
 @app.route('/base')
 def base():
-    return render_template('base.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+    return render_template('base.html')
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        for item in request.form:
+          print(request.form[item])
+    return render_template('form.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        for item in request.form:
+          print(item, request.form[item])
+    return render_template('login.html')
 
 
 
