c4d578770c63e7629d20e9d893bb1eecf4318314
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2024-02-28 00:26:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
 
+users = [
+    {'username': 'kirpich', 'password': 1234}
+]
 
 
 @app.route('/')
@@ -16,15 +19,15 @@ def hello():
         <a href={url_for('login')}>Регистрация</a>
     """
 
+
 @app.route('/index')
 def index():
-    return render_template('index.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+    return render_template('index.html')    # отрисовывает шаблон, лежащий в папке темплейтс
 
 
 @app.route('/start')
 def start():
-    return render_template('start.html')    #отрисовывает шаблон, лежащий в папке темплейтс
-
+    return render_template('start.html')
 
 
 @app.route('/base')
@@ -36,28 +39,34 @@ def base():
 def form():
     if request.method == 'POST':
         for item in request.form:
-          print(request.form[item])
-    return render_template('form.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+            print(request.form[item])
+    return render_template('form.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        for item in request.form:
-          print(item, request.form[item])
+        for user in users:
+            if request.form['login'] == user['username']:
+                if request.form['password'] == user['password']:
+                    return redirect(url_for('profile', username=user['username']))
+        print('Неправильный пароль')
     return render_template('login.html')
 
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('start.html', username=username)
 
 
 @app.route('/day-<num>')
 def day(num):
-    return render_template(f'day-{num}.html')    #отрисовывает шаблон, лежащий в папке темплейтс
+    return render_template(f'day-{num}.html')
 
 
 @app.route('/photo-<num>')
 def photo(num):
     return render_template(f'photo-{num}.html')
-
 
 
 if __name__ == '__main__':
